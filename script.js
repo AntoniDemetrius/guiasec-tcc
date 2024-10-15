dea100660d2739064b3515b1c29f2df3b6d803f0
AntoniDemetrius <antonidemetrius@aluno.fapce.edu.br>
2024-10-15 13:38:19 -0300

    inicio

---
+++ script.js
@@ -0,0 +1,24 @@
+// Botão para ocultar o sidebar 
+const toggler = document.querySelector(".toggler-btn");
+toggler.addEventListener("click", function () {
+  document.querySelector("#sidebar").classList.toggle("collapsed");
+});
+
+function showContent(item) {
+  // Ocultar todos os conteúdos
+  const items = document.querySelectorAll('.content-item');
+  items.forEach(content => {
+      content.style.display = 'none'; // Oculta todos os elementos
+  });
+
+  // Mostrar o conteudo de cada item
+  const selectedItem = document.getElementById(item);
+  if (selectedItem) {
+      selectedItem.style.display = 'block'; // Exibe o conteúdo correspondente
+  }
+}
+
+// Mostrar conteúdo padrão ao carregar a página
+window.onload = function() {
+  showContent('default');
+};
